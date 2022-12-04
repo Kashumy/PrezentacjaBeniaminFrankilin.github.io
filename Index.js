03fa051ee4bcebf3ff59dfa20b51e7c0f40f8d9a
Kashumy <114857637+Kashumy@users.noreply.github.com>
2022-12-04 20:52:16 +0100

    Add files via upload

--- Index.js
+++ Index.js
@@ -1,3 +1,10 @@
+
+		
+
+
+
+
+
 document.getElementById('text1').style.visibility = 'hidden';
 document.getElementById('text2').style.visibility = 'hidden';
 document.getElementById('text3').style.visibility = 'hidden';
@@ -34,4 +41,35 @@ function button3() {
 function button4() {
 	document.getElementById('text7').style.visibility = 'visible';
 
+}
+
+
+
+
+const context = document.querySelector("canvas").getContext("2d")
+const context2 = document.querySelector("canvas").getContext("2d")
+
+context.canvas.height = 400;
+context.canvas.width = 600;
+context2.canvas.height = 400;
+context2.canvas.width = 600;
+
+function draw(){;
+
+context.font = "30px Arial";
+context.fillText
+('Zad 1 wymień 2 rzeczy' , 10, 50);
+context.fillText
+('które wynalazł Ben.Franklin' , 10, 80);
+} 
+function draw2(){
+	context2.font = "15px Arial";
+context2.fillText
+('<  odp.piorunochron, > ' , 10, 110);
+context2.fillText('Zad 2 co dowiódł Benjamin na temat Pioruna ' , 10, 140);
+}
+function draw3(){
+context2.fillText('<  odp.dowiódł że piorun zawiera elektryczność > ' , 10, 170);
+context2.fillText('Zad 3 zapisz wszystkie wynalazki i jego dokonania w ' , 10, 200);
+context2.fillText('Zeszycie!!! ' , 10, 230);
 }
