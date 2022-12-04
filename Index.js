63169972dd307db155188bdfa0a86ffe56c2f923
Kashumy <114857637+Kashumy@users.noreply.github.com>
2022-12-04 11:48:24 +0100

    Ok
    Dobra

---
+++ Index.js
@@ -0,0 +1,37 @@
+document.getElementById('text1').style.visibility = 'hidden';
+document.getElementById('text2').style.visibility = 'hidden';
+document.getElementById('text3').style.visibility = 'hidden';
+document.getElementById('text4').style.visibility = 'hidden';
+document.getElementById('text5').style.visibility = 'hidden';
+document.getElementById('text6').style.visibility = 'hidden';
+document.getElementById('text7').style.visibility = 'hidden';
+document.getElementById('text8').style.visibility = 'hidden';
+
+
+
+
+
+
+
+
+alert ('Prezentacja multimedialna Beniamin Frankilin ( Js Html Css ) zrobiona przez Krystiana Popiołka klasa 8a szkóła 57');
+function button1() {
+	document.getElementById('text1').style.visibility = 'visible';
+	document.getElementById('text2').style.visibility = 'visible';
+}
+function button2() {
+	document.getElementById('text3').style.visibility = 'visible';
+	document.getElementById('text5').style.visibility = 'visible';
+
+}
+function button3() {
+	document.getElementById('text4').style.visibility = 'visible';
+	document.getElementById('text6').style.visibility = 'visible';
+	document.getElementById('text8').style.visibility = 'visible';
+
+
+}
+function button4() {
+	document.getElementById('text7').style.visibility = 'visible';
+
+}
